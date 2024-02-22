63ed00bfd605d31fc942a00e48b11efc7f44f5c5
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-02-22 15:48:42 +0300

    Books migration

---
+++ database/migrations/2024_02_22_085607_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description');
+            $table->string('publisher');
+            $table->year('publish_year');
+            $table->string('ibsn')->unique();
+            $table->string('genre');
+            $table->string('language');
+            $table->integer('page_count');
+            $table->decimal('price', 10, 2);
+            $table->year('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
