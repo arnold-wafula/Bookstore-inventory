f6c6b9fd2226aa245cac06f377c0c53c2e293471
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-02-23 09:11:30 +0300

    Changed the model names according to Laravel conventions

--- app/Http/Controllers/Auth/AdminController.php
+++ app/Http/Controllers/Auth/AdminController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\Roles;
-use App\Models\Countries;
+use App\Models\Role;
+use App\Models\Country;
+use App\Models\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -12,9 +13,27 @@ class AdminController extends Controller
 {
 
     public function index() {
-        $countries = Countries::all();
-        $roles = Roles::all();
+        $countries = Country::all();
+        $roles = Role::all();
 
         return view('admin', compact('countries', 'roles'));
+    }
+
+    public function store() {
+        $user = new User();
+
+        $user->fname = request('fname');
+        $user->lname = request('lname');
+        $user->email = request('email');
+        $user->phone = request('phone');
+        $user->address = request('address');
+        $user->country = request('country');
+        $user->role = request('role');
+
+        $user->password = request('password');
+
+        $user->save();
+
+        return redirect()->route('login');
     }
 }
